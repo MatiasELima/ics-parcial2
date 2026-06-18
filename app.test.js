c8ac0876797c853361b703de505466804ede7ad9
Matias Lima <matiaselima@hotmail.com>
2026-06-18 15:03:21 -0300

    adding test

--- app.test.js
+++ app.test.js
@@ -3,11 +3,17 @@ const express = require('express');
 
 // Creamos una app idéntica para testear
 const app = express();
-app.get('/', (_, res) => res.status(200).send());
+app.get('/', (_, res) => res.status(200).send('<html><title>Calendario</title><div class="calendar"></div></html>'));
 
 describe('Pruebas del Servidor', () => {
   it('Debería responder 200 OK en la ruta raíz', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
+  });
+
+  it('Debería contener el componente del calendario en la respuesta', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toContain('Calendario');
+    expect(response.text).toContain('calendar');
   });
 });
